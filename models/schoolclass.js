cbc4bbeb159069931e48c022e1f746bdb89e643a
ELEXILON <elexilon@gmail.com>
2018-01-24 21:09:45 +0100

    seed student list

--- models/schoolclass.js
+++ models/schoolclass.js
@@ -1,12 +1,16 @@
 const mongoose = require('../config/database')
 const { Schema } = mongoose
 
+const evaluationSchema = new Schema({
+  evaluationColor: { type: String, required: true },
+  createdBy: { type: String, required: true },
+  evaluatedAt: { type: Date, default: Date.now }
+})
+
 const studentSchema = new Schema({
   fullName: { type: String, required: true },
   photo: { type: String, required: true },
-  evaluationColor: { type: String, required: true },
-  previousEvaluationColor: { type: String, required: true },
-  evaluatedAt: { type: Date, default: Date.now }
+  evaluations: [evaluationSchema]
 })
 
 
@@ -15,10 +19,8 @@ const schoolClassSchema = new Schema({
   students: [studentSchema],
   startsAt: { type: Date, default: Date.now, required: true },
   endsAt: { type: Date, default: Date.now, required: true },
-  createdBy: { type: Schema.Types.ObjectId, ref: 'users' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('schoolClasses', schoolClassSchema)
-  
